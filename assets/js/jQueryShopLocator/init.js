e75ac78be22c90dd17989efa81a2914bf5cc5ccb
wnrattweb <61352925+wnrattweb@users.noreply.github.com>
2021-03-14 01:32:54 -0500

    Add files via upload

---
+++ assets/js/jQueryShopLocator/init.js
@@ -0,0 +1,21 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+
+        //init map
+
+        $(".ct-js-googleMap").ShopLocator({
+            pluginStyle: "ct-googlemapStyle",
+            json: "assets/js/jQueryShopLocator/src/json/markers.json",
+            map: {
+                center: [40.712784, -74.005941],
+                zoom: 15,
+                allMarkersInViewport: false,
+                scrollwheel: false,
+                mapStyle: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+            },
+            markersIcon: "assets/images/marker.png"
+        });
+    });
+})(jQuery);
